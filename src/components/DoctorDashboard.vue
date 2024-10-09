ef12fd184210508c9a1acd71bbcafd847cd23847
alessandromagnani <alessandro.magnani18@studio.unibo.it>
2024-10-09 22:10:21 +0200

    Update DoctorDashboard component to retrieve doctor information from Vuex store.

---
+++ src/components/DoctorDashboard.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <h1>Dashboard Dottore</h1>
+    <PatientList :patients="patients" @selectPatient="selectPatient" />
+    <UploadRadiograph
+      v-if="selectedPatient"
+      @uploadSuccess="handleUploadSuccess"
+      :patientId="selectedPatient.id"
+    />
+    <PatientRadiographs
+      :radiographs="selectedPatientRadiographs"
+      v-if="selectedPatient"
+    />
+  </div>
+</template>
+
+<script>
+import PatientList from "./PatientList.vue";
+import PatientRadiographs from "./PatientRadiographs.vue";
+import UploadRadiograph from "./UploadRadiographs.vue";
+import { ref, onMounted } from "vue";
+import { useStore } from "vuex"; // Importa useStore
+import { getRadiographs, getPatients } from "../services/api-service"; // Assicurati che il percorso sia corretto
+
+export default {
+  components: {
+    PatientList,
+    PatientRadiographs,
+    UploadRadiograph,
+  },
+  setup() {
+    const store = useStore(); // Crea un'istanza dello store
+    const patients = ref([]);
+    const selectedPatient = ref(null);
+    const selectedPatientRadiographs = ref([]);
+
+    // Ottieni l'ID del dottore loggato
+    const doctorId = store.state.user.id;
+
+    onMounted(async () => {
+      // Ottieni i pazienti associati
+      patients.value = await getPatients(doctorId);
+    });
+
+    // Seleziona un paziente e ottieni le sue radiografie
+    const selectPatient = async (patient) => {
+      selectedPatient.value = patient;
+      selectedPatientRadiographs.value = await getRadiographs(patient.id);
+    };
+
+    const handleUploadSuccess = () => {
+      selectPatient(selectedPatient.value); // Ricarica le radiografie
+    };
+
+    return {
+      patients,
+      selectPatient,
+      handleUploadSuccess,
+      selectedPatient,
+      selectedPatientRadiographs,
+    };
+  },
+};
+</script>
+
+<style scoped>
+/* Puoi aggiungere qui il tuo stile personalizzato per la dashboard */
+</style>
