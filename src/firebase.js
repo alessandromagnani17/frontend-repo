4b40bc7315ffee850080eaa0c26813e97b269dad
alessandromagnani <alessandro.magnani18@studio.unibo.it>
2024-10-17 14:18:16 +0200

    Transition from AWS Cognito to Google Firebase for user authentication and registration

---
+++ src/firebase.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth"; // Importa il modulo di autenticazione
+import { getFirestore } from "firebase/firestore"; // Importa il modulo Firestore
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBDron87U_PasTdrtOBfnntXUptPNT-3xQ",
+  authDomain: "osteoarthritis-provolone.firebaseapp.com",
+  projectId: "osteoarthritis-provolone",
+  storageBucket: "osteoarthritis-provolone.appspot.com",
+  messagingSenderId: "111916507074",
+  appId: "1:111916507074:web:afaa3c072f75840767005b",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication
+const auth = getAuth(app); // Crea l'oggetto auth
+
+// Initialize Firestore
+const db = getFirestore(app); // Crea l'oggetto db per Firestore
+
+export { auth, db }; // Esporta sia l'oggetto auth che l'oggetto db
