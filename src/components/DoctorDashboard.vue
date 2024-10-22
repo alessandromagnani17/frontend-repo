b73afe03eeb98e1bd6b1c16929b5ac9c95dc217c
alessandromagnani <alessandro.magnani18@studio.unibo.it>
2024-10-22 14:10:57 +0200

    Refactor Doctor Dashboard to Display Patient List and Details

--- src/components/DoctorDashboard.vue
+++ src/components/DoctorDashboard.vue
@@ -1,16 +1,50 @@
 <template>
   <div>
     <h1>Dashboard Dottore</h1>
-    <PatientList :patients="patients" @selectPatient="selectPatient" />
-    <UploadRadiograph
-      v-if="selectedPatient"
-      @uploadSuccess="handleUploadSuccess"
-      :patientId="selectedPatient.id"
-    />
-    <PatientRadiographs
-      :radiographs="selectedPatientRadiographs"
-      v-if="selectedPatient"
-    />
+
+    <div v-if="patients.length > 0">
+      <h2>Elenco Pazienti</h2>
+      <ul>
+        <li
+          v-for="patient in patients"
+          :key="patient.userId"
+          @click="selectPatient(patient)"
+          style="cursor: pointer; text-decoration: underline"
+        >
+          {{ patient.name }} {{ patient.family_name }}
+        </li>
+      </ul>
+    </div>
+    <div v-else>
+      <p>Nessun paziente associato trovato.</p>
+    </div>
+
+    <div v-if="selectedPatient">
+      <h2>Dettagli Paziente</h2>
+      <div>
+        <p><strong>Nome:</strong> {{ selectedPatient.name }}</p>
+        <p><strong>Cognome:</strong> {{ selectedPatient.family_name }}</p>
+        <p><strong>Email:</strong> {{ selectedPatient.email }}</p>
+        <p><strong>Data di nascita:</strong> {{ selectedPatient.birthdate }}</p>
+        <p>
+          <strong>Numero di telefono:</strong>
+          {{ selectedPatient.phone_number }}
+        </p>
+        <p><strong>Indirizzo:</strong> {{ selectedPatient.address }}</p>
+        <!-- Aggiungi qui ulteriori attributi del paziente se necessario -->
+      </div>
+
+      <UploadRadiograph
+        @uploadSuccess="handleUploadSuccess"
+        :patientId="selectedPatient.userId"
+      />
+
+      <PatientRadiographs
+        :radiographs="selectedPatientRadiographs"
+        :patientId="selectedPatient.userId"
+        :patientName="selectedPatient.name"
+      />
+    </div>
   </div>
 </template>
 
@@ -18,7 +52,7 @@
 import PatientRadiographs from "./PatientRadiographs.vue";
 import UploadRadiograph from "./UploadRadiographs.vue";
 import { ref, onMounted } from "vue";
-import { getPatients, getRadiographs } from "../services/api-service"; // Assicurati di importare getPatients
+import { getPatientsFromDoctor, getRadiographs } from "../services/api-service";
 
 export default {
   components: {
@@ -28,17 +62,21 @@ export default {
   setup() {
     const patients = ref([]);
     const selectedPatient = ref(null);
-    const selectedPatientRadiographs = ref([]);
+    const selectedPatientRadiographs = ref([]); // Qui hai definito la proprietà
 
     // Ottieni il DoctorID dal localStorage
     const doctorId = localStorage.getItem("doctorId");
 
     onMounted(async () => {
       console.log("Cerco pazienti associati a: " + doctorId);
-      // Assicurati che doctorId non sia null
       if (doctorId) {
         // Ottieni i pazienti associati
-        patients.value = await getPatients(doctorId);
+        try {
+          patients.value = await getPatientsFromDoctor(doctorId);
+          console.log("patients.value: ", patients.value); // Stampa dopo l'assegnazione
+        } catch (error) {
+          console.error("Errore nel caricamento dei pazienti:", error);
+        }
       } else {
         console.error("Nessun DoctorID trovato nel localStorage.");
       }
@@ -46,8 +84,26 @@ export default {
 
     // Seleziona un paziente e ottieni le sue radiografie
     const selectPatient = async (patient) => {
+      console.log("Paziente selezionato:", patient); // Log del paziente selezionato
       selectedPatient.value = patient;
-      selectedPatientRadiographs.value = await getRadiographs(patient.id);
+
+      // Usa userId al posto di id
+      if (!patient.userId) {
+        console.error("ID paziente non trovato."); // Log dell'errore
+        return;
+      }
+
+      try {
+        // Imposta le radiografie su un array vuoto se non ci sono radiografie
+        selectedPatientRadiographs.value =
+          (await getRadiographs(patient.userId)) || []; // Cambiato da patient.id a patient.userId
+        console.log("Radiografie caricate:", selectedPatientRadiographs.value); // Log delle radiografie caricate
+      } catch (error) {
+        console.error(
+          "Errore nel caricamento delle radiografie per il paziente:",
+          error
+        );
+      }
     };
 
     const handleUploadSuccess = () => {
