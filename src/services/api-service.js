158cc394a7240c19ec62a2cd58ab5004cd8b8f64
alessandromagnani <alessandro.magnani18@studio.unibo.it>
2024-10-23 17:14:03 +0200

    Enhance API service for uploading radiographs

--- src/services/api-service.js
+++ src/services/api-service.js
@@ -26,10 +26,14 @@ export const loginUser = async (loginData) => {
 export const uploadRadiograph = async (patientId, file) => {
   const formData = new FormData();
   formData.append("file", file);
+  formData.append("patientId", patientId); // Aggiunto l'ID paziente ai formData
+  console.log("PatientID: " + patientId);
 
   try {
+    console.log("DOPO DEL TRY");
+
     const response = await axios.post(
-      `${API_URL}/patients/${patientId}/radiographs`,
+      `${API_URL}/api/patients/${patientId}/radiographs`,
       formData,
       {
         headers: {
@@ -39,6 +43,8 @@ export const uploadRadiograph = async (patientId, file) => {
     );
     return response.data;
   } catch (error) {
+    // Aggiungi maggiori dettagli all'errore
+    console.error("Errore durante il caricamento:", error);
     throw new Error(error.response ? error.response.data.error : error.message);
   }
 };
