a5e03a7e1aefab7fdc36ecd0d67feb3af4131290
alessandromagnani <alessandro.magnani18@studio.unibo.it>
2024-11-29 17:57:24 +0100

    Refactor and improve UI for ActivityCalendar

--- src/components/UserLogin.vue
+++ src/components/UserLogin.vue
@@ -579,7 +579,7 @@ h2 {
   left: 0;
   width: 100%;
   height: 100%;
-  background: rgba(255, 255, 255, 0.8);
+  background: rgba(255, 255, 255, 0.6);
   display: flex;
   justify-content: center;
   align-items: center;
